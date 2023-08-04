d079cdf9aad971951353bab962e0897d812588ea
Alexander Drach <alexander-drach@mail.ru>
2023-08-04 11:57:13 +0600

    add component Footer

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,3 +1,5 @@
+import Footer from '../../components/Footer/Footes';
+
 function Favorites(): JSX.Element {
   return (
     <div className="page">
@@ -159,11 +161,7 @@ function Favorites(): JSX.Element {
           </section>
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-        </a>
-      </footer>
+      <Footer/>
     </div>
   );
 }
